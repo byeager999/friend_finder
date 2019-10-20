e53066c68d3d0da5c391d4d8c2d65ee47764ac7e
Brian Yeager <rbrianyeager@gmail.com>
2019-10-19 20:51:23 -0400

    Friend is added to the array need to work on the matching logic

--- server.js
+++ server.js
@@ -3,8 +3,7 @@
 var express = require("express");
 var path = require("path");
 var app = express();
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+
 
 // Sets up the Express App
 // =============================================================
@@ -12,12 +11,16 @@ require("./routing/htmlRoutes")(app);
 var PORT = 3040;
 
 // Sets up the Express app to handle data parsing
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-  
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
