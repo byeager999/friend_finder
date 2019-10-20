3e8db603e7d887d6f78bf6fc6af1afe81a3c162a
Brian Yeager <rbrianyeager@gmail.com>
2019-10-19 22:04:35 -0400

    Friend added, modal works, but is not comparing against the array

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -6,69 +6,38 @@ module.exports = function (app) {
         res.json(friends);
     });
     app.post("/api/friends", function(req, res) {
-        friends.push(req.body);
-        res.json(true);
-    });
+        var userScore = req.body.scores;
+        var scoresArr = [];
+        var bestMatch = 0;
 
-    // Route that handles the incoming survey data and the compatibility logic
-    // Create New Friend - takes in JSON input
-    app.post("/api/friends", function (req, res) {
-        var difference = 0;
-        var bestMatch = {
-            name: "",
-            photo: "",
-            scoreDifference: 1000
-        };
-        var userData = req.body;
-        var userName = userData.name;
-        var userScores = userData.scores;
+        for(var i = 0; i < friends.length; i++) {
+            var scoreDiff = 0;
 
-        var b = userScores.map(function(item) {
-            return parseInt(item, 10);
-        });
-        userData = {
-            name: req.body.name,
-            photo: req.body.photo,
-            scores: b
-        };
-
-        console.log("Name: " + userName);
-        console.log("User score: " + userScores);
-
-        var sum = b.reduce((a, b) => a + b, 0);
-        console.log("Sum of users score: " + sum);
-        console.log("Best match difference: " + bestMatch.scoreDifference);
-
-        for(i = 0; i < friends.length; i++) {
-            console.log(friends[i].name);
-            difference = 0;
-            console.log("Total difference " + difference);
-            console.log("Best match difference: " + bestMatch.scoreDifference);
-
-            var bestScore = friends[i].scores.reduce((a, b) => a + b, 0);
-            console.log("Total friend score " + bestScore);
-            difference += Math.abs(sum - bestScore);
-            console.log("Score difference: " + difference);
-
-            if (difference <= bestMatch.scoreDifference){
-                bestMatch.name = friends[i].name;
-                bestMatch.photo = friends[i].photo;
-                bestMatch.scoreDifference = difference;
+            for(var j = 0; j < userScore.length; j++) {
+                scoreDiff += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(userScore[j])))
             }
-            console.log(difference = " total score difference");
-
+            scoresArr.push(scoreDiff);
+            
         }
+        for (var i = 0; i < scoresArr.length; i++) {
+            if (scoresArr[i] <= scoresArr[bestMatch]) {
+                bestMatch = i;
+            }
+        }
+        var match = friends[bestMatch];
+        res.json(match);
 
-        console.log(bestMatch);
-        friends.push(userData);
-        console.log("New User added");
-        console.log(userData);
-        res.json(bestMatch);
-
-        
+        console.log(req.body);
+        friends.push(req.body)
     });
 
-
+    app.post("/api/clear", function(req, res) {
+        friends.length = [];
+        res.json({
+            ok: true
+        });
+    });
+    
 
 
 };
